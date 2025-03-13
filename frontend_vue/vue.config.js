a18e258a65951b0bd270b3bde7e37846aa82e7fd
ikki0 <arojas1297@gmail.com>
2025-03-13 23:25:14 +0100

    new configuration to use ports to show page on internet

--- frontend_vue/vue.config.js
+++ frontend_vue/vue.config.js
@@ -1,8 +1,22 @@
 const { defineConfig } = require('@vue/cli-service')
+
+// RECUPERAR VALOR .ENV USE_PORTS_HTTPS PARA HABILITAR PUERTO HTTPS
+const useHttps = process.env.USE_PORTS_HTTPS === 'true'
+
 module.exports = defineConfig({
     transpileDependencies: true,
     publicPath: process.env.NODE_ENV === 'production'
         ? '/your-production-sub-path/'
         : '/',
     // Asegúrate de no tener configuraciones que forcen el uso del modo hash
+    devServer: useHttps ? {
+        https: true,
+        client: {
+            webSocketURL: {
+                protocol: 'wss', // Fuerza el uso de WebSocket seguro
+                hostname: 'localhost', // Ajusta el hostname según sea necesario
+                port: 8080, // Asegúrate de que coincida con el puerto que usas
+            }
+        }
+    } : {}
 })
